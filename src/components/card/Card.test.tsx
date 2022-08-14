04f2135dbbdde004a090f5ae8bb96144407b83c3
Hector Zurga <hjzurga@gmail.com>
2022-08-14 19:31:13 -0400

    Fixed tests

--- src/components/card/Card.test.tsx
+++ src/components/card/Card.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Card } from './Card';
 
 describe('Card component', () => {
   test('should render name and url', () => {
     render(<Card name="name" url="url" />);
+
+    const cardContainer =
+      screen.getByText('name').parentElement?.parentElement ?? new Element();
+    userEvent.click(cardContainer);
 
     expect(screen.getByText('name')).toBeInTheDocument();
     expect(screen.getByText('url')).toBeInTheDocument();
